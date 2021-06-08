8dcd1ed588617d034d04ed4aeee827349757244f
Luana Giusto <luanagiusto@gmail.com>
2021-06-08 10:52:29 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { SharedModule } from './shared/shared.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+registerLocaleData(localePt);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    DashboardModule,
+    AppRoutingModule,
+    SharedModule,
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID, useValue: 'pt-BR'
+    }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
