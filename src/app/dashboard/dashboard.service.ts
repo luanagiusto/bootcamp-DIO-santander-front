1c5a689a748cbaf9c6cae5baea4ca7fc3af6839c
Luana Giusto <luanagiusto@gmail.com>
2021-06-09 12:51:18 -0300

    atualização do link heroku

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -8,8 +8,8 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
 
-  readonly baseUrl = 'https://dio-santander-giusto.herokuapp.com';
- 
+  readonly baseUrl = 'https://dio-santander-giusto.herokuapp.com/bootcamp';
+
 
   constructor(private http: HttpClient) { }
 
